dba52315cdf54c6fec8ef3bed45e7b1e1fa54ff4
Manimi The Fire H/R <manimifire@gmail.com>
2018-08-04 13:23:11 +0300

    Update depression.py

--- depression.py
+++ depression.py
@@ -1482,7 +1482,7 @@ async def gem(ctx, membername=None, xpos : int=None, ypos : int=None):
 	
             backgroundd.paste(gemmm, (x,y), gemmm)
 
-            backgroundd.paste(auraa, (x,y), mask)
+            backgroundd.paste(auraa, (x,y), auraa)
     
             backgroundd.save("gempic.png", "PNG")
     
